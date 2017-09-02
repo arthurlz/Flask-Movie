68ae878389b1cda3d2f88c14a4ecee84a9a52226
loveppears <litar929@gmail.com>
2017-09-02 23:09:44 +0900

    refractor app/__init__.py

--- app/models.py
+++ app/models.py
@@ -1,15 +1,8 @@
 # coding: utf8
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
-import pymysql
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:3306/movie"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
-db = SQLAlchemy(app)
+from app import db
 
 
 # 会员
